406ca99db44d8025097af914fa89fcb74057bf39
wfalby4257 <walter.falby@bsu.edu>
2025-04-01 19:17:09 +0000

    Latest changes

--- KoffeeKountProject/KoffeeKount/Program.cs
+++ KoffeeKountProject/KoffeeKount/Program.cs
@@ -1,9 +1,31 @@
 ﻿namespace KoffeeKount;
 
+using System.IO;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string koffeePrice = getKoffeePrice();
+        // Write the text to the console.
+        Console.WriteLine(koffeePrice);
+
+        //Write main menu to console
+        
+    }
+
+    public static string getKoffeePrice() {
+        try
+        {
+            // Open the text file using a stream reader.
+            using StreamReader reader = new("KoffeePrice.txt");
+
+            // Read the stream as a string.
+            return(reader.ReadLine());
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+            return("The file could not be read!");
+        }
     }
 }
