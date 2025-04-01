e527d3a5ae7ad14dc975acd523b3836bc2fc790e
wfalby4257 <walter.falby@bsu.edu>
2025-04-01 20:59:33 +0000

    Last for today

---
+++ KoffeeKountProject/KoffeeKount/Domain.cs
@@ -0,0 +1,15 @@
+namespace KoffeeKount;
+using System;
+public class Koffee {
+    public DateTime dateAdded;
+    public double price;
+    public int count;
+    public string type;
+    public int dayNumber;
+    
+}
+
+public class Reminder {
+    public string dateAdded;
+    
+}
