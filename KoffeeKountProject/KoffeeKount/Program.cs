36a51e1f7d9bf9e20028dd7380c089fb00a00a2c
wfalby4257 <walter.falby@bsu.edu>
2025-04-02 20:59:01 +0000

    Latest updates 6

--- KoffeeKountProject/KoffeeKount/Program.cs
+++ KoffeeKountProject/KoffeeKount/Program.cs
@@ -16,8 +16,8 @@ class Program
         Console.WriteLine("Select one of the following actions (use the number):");
         Console.WriteLine(String.Join(Environment.NewLine, selections));
 
-        string selected = Console.ReadLine();
-        switch (selected) {
+        string selection = Console.ReadLine();
+        switch (selection) {
             case "1":
                 Console.WriteLine("Buy a Koffee was selected.");
                 buyAKoffee(koffeePrice, koffeeFH);
@@ -25,6 +25,7 @@ class Program
 
             case "2":
                 Console.WriteLine("Get Koffee count was selected.");
+                getKoffeeCount(koffeeFH);
                 break;
 
             case "3":
@@ -49,10 +50,11 @@ class Program
 
             case "8":
                 Console.WriteLine("Reset files was selected.");
+                resetFiles(koffeeFH);
                 break;
 
             case "99":
-                Console.WriteLine("Exit was selected.");
+                Console.WriteLine("Leo has left the building.");
                 break;
 
             default:
@@ -69,33 +71,30 @@ class Program
         Console.WriteLine(String.Join(Environment.NewLine, types));
 
         string type = Console.ReadLine();
+        Koffee cupOfKoffee = null;
         switch (type) {
             case "1":
                 Console.WriteLine("Americano selected");
-                Koffee cupOfKoffee = new Koffee(double.Parse(koffeePrice), 1, "Americano");
-                cupOfKoffee.showKoffee();
-                try {
-                    koffeeFH.writeKoffeeInfo(cupOfKoffee);
-                }
-                catch (ArgumentException ex) {
-                    Console.WriteLine("Failed writing Koffee data!");
-                    Console.WriteLine(ex.Message);
-                }                
+                cupOfKoffee = new Koffee(double.Parse(koffeePrice), 1, "Americano");
                 break;
 
             case "2":
+                cupOfKoffee = new Koffee(double.Parse(koffeePrice), 1, "Cappuccino");    
                 Console.WriteLine("Cappuccino selected");
                 break;
 
             case "3":
+                cupOfKoffee = new Koffee(double.Parse(koffeePrice), 1, "DoubleDouble");
                 Console.WriteLine("DoubleDouble selected");
                 break;
 
             case "4":
+                cupOfKoffee = new Koffee(double.Parse(koffeePrice), 1, "Expresso");
                 Console.WriteLine("Expresso selected");
                 break;
 
             case "5":
+                cupOfKoffee = new Koffee(double.Parse(koffeePrice), 1, "Latte");
                 Console.WriteLine("Latte selected");
                 break;
 
@@ -103,21 +102,58 @@ class Program
                 Console.WriteLine("Invalid selection!");
                 break;
         }
+
+        cupOfKoffee.showKoffee();
+        try {
+            koffeeFH.writeKoffeeInfo(cupOfKoffee);
+        }
+        catch (ArgumentException ex) {
+            Console.WriteLine("Failed writing Koffee data!");
+            Console.WriteLine(ex.Message);
+        }            
     }
 
-    public static string getKoffeePrice() {
-        try
-        {
-            // Open the text file using a stream reader.
-            using StreamReader reader = new("KoffeePrice.txt");
+    public static void getKoffeeCount(KoffeeFileHandler koffeeFH) {
+        Console.WriteLine("Enter range in number of days: ");
+        int days = int.Parse(Console.ReadLine());
+        try {
+            koffeeFH.getKoffeeCount();
+        }
+        catch (FileNotFoundException ex) {
+            Console.WriteLine(" You have not made any Koffee purchases in the last " + days + " days.");
+        }
+    }
 
-            // Read the stream as a string.
-            return(reader.ReadLine());
+    public static void resetFiles(KoffeeFileHandler koffeeFH) {
+        var options = new [] {" 1 All", " 2 Log entries", " 3 Reminders", " 4 Koffee"};
+
+        //Write menu
+        Console.WriteLine("Make a selection (use the number):");
+        Console.WriteLine(String.Join(Environment.NewLine, options));
+
+        string option = Console.ReadLine();
+        switch(option) {
+            case "1":
+                Console.WriteLine("All files to be deleted");
+                break;
+
+            case "2":
+                Console.WriteLine("Log entries file to be deleted");
+                break;
+
+            case "3":
+                Console.WriteLine("Reminders file to be deleted");
+                break;
+
+            case "4":
+                Console.WriteLine("Koffee purchases file to be deleted");
+                koffeeFH.deleteKoffeeFile();
+                break;
+
+            default:
+                Console.WriteLine("Invalid selection! Try Again.");
+                break;
         }
-        catch (IOException e)
-        {
-            Console.WriteLine(e.Message);
-            return("The file could not be read!");
-        }
+
     }
 }
