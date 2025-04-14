c83e2001868d724e89d53fbf72016e072876777d
wfalby4257 <walter.falby@bsu.edu>
2025-04-14 16:14:49 +0000

    Refactor updates

---
+++ KoffeeKountProject/KoffeeKount/KoffeeUI.cs
@@ -0,0 +1,143 @@
+namespace KoffeeKount;
+using System.Text.RegularExpressions;
+
+public class KoffeeUI {
+
+    public KoffeeFileHandler koffeeFH;
+    string koffeePrice;
+
+    public KoffeeUI () {
+        //Get the base price for cup of Koffee
+    this.koffeeFH = new KoffeeFileHandler();
+    this.koffeePrice = koffeeFH.getKoffeePrice();
+    }
+
+    public void buyAKoffee(string koffeePrice, KoffeeFileHandler koffeeFH) {
+        var types = new [] {" 1 Americano", " 2 Cappuccino", " 3 DoubleDouble", " 4 Expresso", " 5 Latte"};
+        string type = "";
+        string koffeeType = "";
+        char typeSelected = 'N';
+
+        Console.WriteLine("Entered new class.");
+
+        do {
+            //Write menu
+            Console.WriteLine("Make a selection (use the number). Enter Exit to terminate.");
+            Console.WriteLine(String.Join(Environment.NewLine, types));
+
+            type = Console.ReadLine() ?? string.Empty;        
+            if (String.IsNullOrEmpty(type)) {
+                Console.WriteLine("The input could not be read!");
+                //Prepare to exit the method
+                type = "Exit";
+            }
+
+            switch (type) {
+                case "1":
+                    koffeeType = "Americano";                
+                    typeSelected = 'Y';
+                    break;
+
+                case "2":
+                    koffeeType = "Cappuccino";                
+                    typeSelected = 'Y';
+                    break;
+
+                case "3":
+                    koffeeType = "DoubleDouble";                
+                    typeSelected = 'Y';
+                    break;
+
+                case "4":                
+                    koffeeType = "Expresso";
+                    typeSelected = 'Y';
+                    break;
+
+                case "5":
+                    koffeeType = "Latte";            
+                    typeSelected = 'Y';    
+                    break;
+
+                case "Exit":
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid selection! Try again.");
+                    break;
+            }
+        } while (typeSelected == 'N');
+
+        if (typeSelected == 'Y') {
+            //Reset flag for next selection
+            typeSelected = 'N';
+
+            Koffee cupOfKoffee = new Koffee(double.Parse(koffeePrice), 1, koffeeType);
+            //cupOfKoffee.showKoffee();
+            try {
+                koffeeFH.writeKoffeeInfo(cupOfKoffee);
+                Console.WriteLine("You've purchased a cup of " + koffeeType + " for " + koffeePrice + ".");
+            }
+            catch (ArgumentException ex) {
+                Console.WriteLine("Failed writing Koffee data!");
+                Console.WriteLine(ex.Message);
+            
+            }
+        }            
+    }
+
+    public void getKoffeeCount(KoffeeFileHandler koffeeFH) {
+        int totalKoffeeCount = 0;
+        int days = 0;
+        Console.WriteLine("Entered new class.");
+
+        Console.WriteLine("Enter range in number of days: ");
+        try {
+            days = int.Parse(Console.ReadLine() ?? string.Empty);            
+            totalKoffeeCount = koffeeFH.getKoffeeCount(days);
+        }
+        catch (FileNotFoundException ex) {
+            Console.WriteLine("You have not made any Koffee purchases in the last " + days + " days.");
+            return;
+        }
+        catch (FormatException ex) {
+            Console.WriteLine("Enter a valid number of days.");
+            //Console.WriteLine(ex.Message);
+            return;
+        }
+
+        if (totalKoffeeCount > 0) {
+            Console.WriteLine("Number of Koffees purchased in the last " + days + " days: " + totalKoffeeCount); 
+        }
+        else {
+            Console.WriteLine("You have not made any Koffee purchases in the last " + days + " days.");
+        }
+    }
+
+    public void setBaseKoffeePrice(KoffeeFileHandler koffeeFH) {
+        char validPrice = 'N';
+        string price = "";
+        string pattern = @"^[0-9]*\.?[0-9]+$";
+        Console.WriteLine("Entered new class.");
+
+        do {
+            Console.WriteLine("Enter base Koffee price. Format is 9.99 (Dollars and cents) ");
+            price = Console.ReadLine() ?? string.Empty;
+            if (String.IsNullOrEmpty(price)) {
+                Console.WriteLine("The price could not be read! Try again.");       
+                continue;         
+            }
+
+            //Make sure only numbers and period
+            if (Regex.IsMatch(price, pattern)) {
+                validPrice = 'Y';
+            }            
+            else {
+                Console.WriteLine("Invalid input! Try again.");
+            }
+        } while (validPrice == 'N');
+
+        //Valid price entered, save it.
+        koffeeFH.setBaseKoffeePrice(price);
+        Console.WriteLine("Base price " + price + " saved.");
+    }
+}
--- KoffeeKountProject/KoffeeKount/Program.cs
+++ KoffeeKountProject/KoffeeKount/Program.cs
@@ -14,6 +14,8 @@ class Program
         KoffeeFileHandler koffeeFH = new KoffeeFileHandler();
         string koffeePrice = koffeeFH.getKoffeePrice();
 
+        KoffeeUI koffeeUI = new KoffeeUI();
+
         LogFileHandler logFH = new LogFileHandler();
 
         ReminderFileHandler reminderFH = new ReminderFileHandler();
@@ -26,10 +28,10 @@ class Program
             selection = Console.ReadLine() ?? string.Empty;
             Console.Clear();
             if (selection == "1") {
-                buyAKoffee(koffeePrice, koffeeFH);
+                koffeeUI.buyAKoffee(koffeePrice, koffeeFH);
             }
             else if (selection == "2") {
-                getKoffeeCount(koffeeFH);
+                koffeeUI.getKoffeeCount(koffeeFH);
             }
             else if (selection == "3") {
                 writeReminderEntry(reminderFH);
@@ -44,7 +46,7 @@ class Program
                 listLogEntries(logFH);
             }
             else if (selection == "7") {
-                setBaseKoffeePrice(koffeeFH);
+                koffeeUI.setBaseKoffeePrice(koffeeFH);
             }
             else if (selection == "8") {
                 resetFiles(koffeeFH, logFH, reminderFH);
@@ -56,128 +58,74 @@ class Program
         } while (selection != "99");
     }
 
-    public static void buyAKoffee(string koffeePrice, KoffeeFileHandler koffeeFH) {
-        var types = new [] {" 1 Americano", " 2 Cappuccino", " 3 DoubleDouble", " 4 Expresso", " 5 Latte"};
-        string type = "";
-        string koffeeType = "";
-        char typeSelected = 'N';
 
-        do {
-            //Write menu
-            Console.WriteLine("Make a selection (use the number). Enter Exit to terminate.");
-            Console.WriteLine(String.Join(Environment.NewLine, types));
+    public static void writeReminderEntry(ReminderFileHandler reminderFH) {        
+        int alertIntrvl = 0;
 
-            type = Console.ReadLine() ?? string.Empty;        
-            if (String.IsNullOrEmpty(type)) {
-                Console.WriteLine("The input could not be read!");
-                //Prepare to exit the method
-                type = "Exit";
-            }
-
-            switch (type) {
-                case "1":
-                    koffeeType = "Americano";                
-                    typeSelected = 'Y';
-                    break;
-
-                case "2":
-                    koffeeType = "Cappuccino";                
-                    typeSelected = 'Y';
-                    break;
-
-                case "3":
-                    koffeeType = "DoubleDouble";                
-                    typeSelected = 'Y';
-                    break;
-
-                case "4":                
-                    koffeeType = "Expresso";
-                    typeSelected = 'Y';
-                    break;
-
-                case "5":
-                    koffeeType = "Latte";            
-                    typeSelected = 'Y';    
-                    break;
-
-                case "Exit":
-                    return;
-
-                default:
-                    Console.WriteLine("Invalid selection! Try again.");
-                    break;
-            }
-        } while (typeSelected == 'N');
-
-        if (typeSelected == 'Y') {
-            //Reset flag for next selection
-            typeSelected = 'N';
-
-            Koffee cupOfKoffee = new Koffee(double.Parse(koffeePrice), 1, koffeeType);
-            //cupOfKoffee.showKoffee();
-            try {
-                koffeeFH.writeKoffeeInfo(cupOfKoffee);
-                Console.WriteLine("You've purchased a cup of " + koffeeType + " for " + koffeePrice + ".");
-            }
-            catch (ArgumentException ex) {
-                Console.WriteLine("Failed writing Koffee data!");
-                Console.WriteLine(ex.Message);
-            
-            }
-        }            
-    }
-
-    public static void getKoffeeCount(KoffeeFileHandler koffeeFH) {
-        int totalKoffeeCount = 0;
-        int days = 0;
-
-        Console.WriteLine("Enter range in number of days: ");
-        try {
-            days = int.Parse(Console.ReadLine() ?? string.Empty);            
-            totalKoffeeCount = koffeeFH.getKoffeeCount(days);
-        }
-        catch (FileNotFoundException ex) {
-            Console.WriteLine("You have not made any Koffee purchases in the last " + days + " days.");
-        }
-        catch (FormatException ex) {
-            Console.WriteLine("Enter a valid number of days.");
-            //Console.WriteLine(ex.Message);
+        Console.WriteLine("Enter reminder title: ");
+        string title = Console.ReadLine() ?? string.Empty;        
+        if (String.IsNullOrEmpty(title)) {            
+            Console.WriteLine("Enter a title.");
             return;
         }
 
-        if (totalKoffeeCount > 0) {
-            Console.WriteLine("Number of Koffees purchased in the last " + days + " days: " + totalKoffeeCount); 
+        Console.WriteLine("Enter reminder trigger date (MM/DD/YYYY): ");
+        string triggerDate = Console.ReadLine() ?? string.Empty;        
+        if (String.IsNullOrEmpty(triggerDate)) {            
+            Console.WriteLine("Enter a date value.");
+            return;
         }
-        else {
-            Console.WriteLine(" You have not made any Koffee purchases in the last " + days + " days.");
+
+        Console.WriteLine("Enter reminder trigger time (HH:MM AM/PM): ");
+        string triggerTime = Console.ReadLine() ?? string.Empty;
+        if (String.IsNullOrEmpty(triggerDate)) {
+            Console.WriteLine("Enter a time value.");
+            return;
+        }
+
+        Console.WriteLine("Enter an alert interval (optional): ");
+        string tempVar = Console.ReadLine() ?? string.Empty;
+        if (!String.IsNullOrEmpty(tempVar)) {
+            alertIntrvl = int.Parse(tempVar);                
+        }
+
+        Console.WriteLine("Enter a reminder note (optional): ");
+        string note = Console.ReadLine() ?? string.Empty;
+        
+        Reminder reminder = new Reminder(title, triggerDate, triggerTime, alertIntrvl, note); 
+        try {
+            reminderFH.writeReminderEntry(reminder);
+            Console.WriteLine("Reminder: " + title + " set for " + triggerDate + " at " + triggerTime + ".");
+        }
+        catch (ArgumentException ex) {
+            Console.WriteLine(ex.Message);
         }
     }
 
-    public static void setBaseKoffeePrice(KoffeeFileHandler koffeeFH) {
-        char validPrice = 'N';
-        string price = "";
-        string pattern = @"^[0-9]*\.?[0-9]+$";
+    public static void writeLogEntry(LogFileHandler logFH) {
+        Console.WriteLine("Enter Log entry.");
 
-        do {
-            Console.WriteLine("Enter base Koffee price. Format is 9.99 (Dollars and cents) ");
-            price = Console.ReadLine() ?? string.Empty;
-            if (String.IsNullOrEmpty(price)) {
-                Console.WriteLine("The price could not be read! Try again.");       
-                continue;         
-            }
+        string logData = Console.ReadLine() ?? string.Empty;
+        if (String.IsNullOrEmpty(logData)) {
+                return;
+        }
 
-            //Make sure only numbers and period
-            if (Regex.IsMatch(price, pattern)) {
-                validPrice = 'Y';
-            }            
-            else {
-                Console.WriteLine("Invalid input! Try again.");
-            }
-        } while (validPrice == 'N');
+        try {
+            logFH.writeLogEntry(logData);
+            Console.WriteLine("Log entry saved.");
+        }
+        catch (ArgumentException ex) {
+            Console.WriteLine(ex.Message);
+        }
+    }
 
-        //Valid price entered, save it.
-        koffeeFH.setBaseKoffeePrice(price);
-        Console.WriteLine("Base price " + price + " saved.");
+    public static void listReminderEntries(ReminderFileHandler reminderFH) {
+        try {
+            reminderFH.listReminderEntries();
+        }
+        catch (ArgumentException ex) {
+            Console.WriteLine(ex.Message);
+        }    
     }
 
     public static void listLogEntries(LogFileHandler logFH) {
@@ -234,74 +182,5 @@ class Program
                     break;
             }
         } while (option != "Exit");
-    }
-
-    public static void writeLogEntry(LogFileHandler logFH) {
-        Console.WriteLine("Enter Log entry.");
-
-        string logData = Console.ReadLine() ?? string.Empty;
-        if (String.IsNullOrEmpty(logData)) {
-                return;
-        }
-
-        try {
-            logFH.writeLogEntry(logData);
-            Console.WriteLine("Log entry saved.");
-        }
-        catch (ArgumentException ex) {
-            Console.WriteLine(ex.Message);
-        }
-    }
-
-    public static void writeReminderEntry(ReminderFileHandler reminderFH) {        
-        int alertIntrvl = 0;
-
-        Console.WriteLine("Enter reminder title: ");
-        string title = Console.ReadLine() ?? string.Empty;        
-        if (String.IsNullOrEmpty(title)) {            
-            Console.WriteLine("Enter a title.");
-            return;
-        }
-
-        Console.WriteLine("Enter reminder trigger date (MM/DD/YYYY): ");
-        string triggerDate = Console.ReadLine() ?? string.Empty;        
-        if (String.IsNullOrEmpty(triggerDate)) {            
-            Console.WriteLine("Enter a date value.");
-            return;
-        }
-
-        Console.WriteLine("Enter reminder trigger time (HH:MM AM/PM): ");
-        string triggerTime = Console.ReadLine() ?? string.Empty;
-        if (String.IsNullOrEmpty(triggerDate)) {
-            Console.WriteLine("Enter a time value.");
-            return;
-        }
-
-        Console.WriteLine("Enter an alert interval (optional): ");
-        string tempVar = Console.ReadLine() ?? string.Empty;
-        if (!String.IsNullOrEmpty(tempVar)) {
-            alertIntrvl = int.Parse(tempVar);                
-        }
-
-        Console.WriteLine("Enter a reminder note (optional): ");
-        string note = Console.ReadLine() ?? string.Empty;
-        
-        Reminder reminder = new Reminder(title, triggerDate, triggerTime, alertIntrvl, note); 
-        try {
-            reminderFH.writeReminderEntry(reminder);
-            Console.WriteLine("Reminder: " + title + " set for " + triggerDate + " at " + triggerTime + ".");
-        }
-        catch (ArgumentException ex) {
-            Console.WriteLine(ex.Message);
-        }
-    }
-
-    public static void listReminderEntries(ReminderFileHandler reminderFH) {
-        try {
-            reminderFH.listReminderEntries();
-        }
-        catch (ArgumentException ex) {
-            Console.WriteLine(ex.Message);
-        }    
     }
 }
