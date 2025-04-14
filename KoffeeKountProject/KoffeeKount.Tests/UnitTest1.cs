c2502a2c5152e9311cb73d047ca62800f4b10fab
wfalby4257 <walter.falby@bsu.edu>
2025-04-14 22:20:14 +0000

    First test

--- KoffeeKountProject/KoffeeKount.Tests/UnitTest1.cs
+++ KoffeeKountProject/KoffeeKount.Tests/UnitTest1.cs
@@ -7,6 +7,11 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        string logFileName = "LogEntries.txt";
+        LogFileHandler logFH = new LogFileHandler(logFileName);
+
+        char msgFlag = 'N';
+        logFH.deleteLogFile(msgFlag);
 
     }
 }
