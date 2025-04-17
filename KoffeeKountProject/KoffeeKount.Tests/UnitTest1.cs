d20d28289bcbac604027a680fb17e17e6c18be18
wfalby4257 <walter.falby@bsu.edu>
2025-04-17 13:08:12 +0000

    First tests

--- KoffeeKountProject/KoffeeKount.Tests/UnitTest1.cs
+++ KoffeeKountProject/KoffeeKount.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 namespace KoffeeKount.Tests;
 
+using System.IO;
 using KoffeeKount;
 
 public class UnitTest1
@@ -7,11 +8,23 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        string logFileName = "LogEntries.txt";
+        string logFileName = "TestLogEntries.txt";
+
+        //Always start with a new file
+        if (File.Exists(logFileName)) {
+            File.Delete(logFileName);
+        }
+
         LogFileHandler logFH = new LogFileHandler(logFileName);
 
-        char msgFlag = 'N';
-        logFH.deleteLogFile(msgFlag);
+        string tmpStr = "This is a test log entry.";
+        logFH.writeLogEntry(tmpStr);
 
+        Assert.True(File.Exists(logFileName));
+
+        string logData = File.ReadAllText(logFileName);
+        string [] logEntries = logData.Split(';');
+        string cmpStr = logEntries[1].Trim(';');
+        Assert.Equal(tmpStr, cmpStr);
     }
 }
